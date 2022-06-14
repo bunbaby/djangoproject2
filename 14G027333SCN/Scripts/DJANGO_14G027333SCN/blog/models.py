145d54b9f47e99629bab42a73160065ec82c91b9
bunbaby <79358888+bunbaby@users.noreply.github.com>
2022-06-14 17:36:14 +0100

    Update models.py

--- 14G027333SCN/Scripts/DJANGO_14G027333SCN/blog/models.py
+++ 14G027333SCN/Scripts/DJANGO_14G027333SCN/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class post(models.Model):
     title = models.CharField(max_length= 200)
+    author = get_user_model(Foreign key)
     text = models.TextField(max_length=100)
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
